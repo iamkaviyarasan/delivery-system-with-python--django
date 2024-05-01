ef50eb876bd04b5cbd594ce617bd59827be39c97
iamkaviyarasan <kaviyarasangunasekaran@gmail.com>
2024-05-01 15:11:30 +0530

    login bug 8

--- fastparcel/fastparcel/settings.py
+++ fastparcel/fastparcel/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_URL ='/sign-in/'
+LOGIN_REDIRECT_URL='/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
